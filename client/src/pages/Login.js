5599cbcf6ad7389b35704504959ebcb7700fb2fb
jaspreet kaur <kaurjas1928@yahoo.com>
2023-02-13 18:09:09 +0000

    added useState

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,13 +1,31 @@
 
-
-export const Login = () => {
+import React, { useState } from "react";
+ export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  
   return (
-    <form>
-      <label htmlFor="email">email</label>
-        <input type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-    </form>
-
-  );
+		<form>
+			<label htmlFor="email">email</label>
+			<input
+        value={email}
+				type="email"
+				placeholder="youremail@gmail.com"
+				id="email"
+				name="email"
+			/>
+			<label htmlFor="password">password</label>
+			<input
+      value={pass}
+				type="password"
+				placeholder="********"
+				id="password"
+				name="password"
+			/>
+      <button>Log In</button>
+		</form>
+	);
 };
+
 
 
