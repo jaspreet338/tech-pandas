a7b63b25f36818d46b34c762ad8aa9328153c10a
jaspreet kaur <kaurjas1928@yahoo.com>
2023-02-13 18:24:09 +0000

    added button and fix the error

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,27 +3,36 @@ import React, { useState } from "react";
  export const Login = () => {
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
-  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+  };
   return (
-		<form>
-			<label htmlFor="email">email</label>
-			<input
-        value={email}
-				type="email"
-				placeholder="youremail@gmail.com"
-				id="email"
-				name="email"
-			/>
-			<label htmlFor="password">password</label>
-			<input
-      value={pass}
-				type="password"
-				placeholder="********"
-				id="password"
-				name="password"
-			/>
-      <button>Log In</button>
-		</form>
+		<>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="email">email</label>
+				<input
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					type="email"
+					placeholder="youremail@gmail.com"
+					id="email"
+					name="email"
+				/>
+				<label htmlFor="password">password</label>
+				<input
+					value={pass}
+					onChange={(e) => setPass(e.target.value)}
+					type="password"
+					placeholder="********"
+					id="password"
+					name="password"
+				/>
+				<button type="submit">Log In</button>
+			</form>
+			<button>Not have an account? Register here.</button>
+		</>
 	);
 };
 
